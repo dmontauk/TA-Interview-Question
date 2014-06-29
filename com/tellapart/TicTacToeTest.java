519ba52282d9e93ab0aaeff61a1f8ae13ca1f9ec
dmontauk <dobromirv@dobromirv-macbookpro2.roam.corp.google.com>
2014-06-28 22:50:09 -0700

    Trying to add tests and failing.

---
+++ com/tellapart/TicTacToeTest.java
@@ -0,0 +1,20 @@
+package com.tellapart;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/** A first test for the TicTacToe program. This is pretty futile since the program is structured
+ * in a very hard-to-test way, but it's a start.
+ * @author dobromirv
+ */
+public class TicTacToeTest {
+
+	@Test
+	public void testSimpleMoveStrategy() {
+		TicTacToe fakeGame = new TicTacToe();
+		// Oh no, this already prints to System.out and waits for user input!
+	}
+
+}
+
